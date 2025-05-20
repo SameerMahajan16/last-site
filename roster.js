3e79c35f3c6e5b9712c100352eb9878fbfb27705
sameer16 <Samneomah@gmail>
2025-05-20 09:31:23 -0400

    tennis update

--- roster.js
+++ roster.js
@@ -1,142 +1,107 @@
 const players = [
-    {
-      firstName: 'Francisco',
-      lastName: 'Lindor',
-      position: 'SS',
-      age: 31,
-      photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/32129.png&w=350&h=254'
-    },
-    {
-      firstName: 'Pete',
-      lastName: 'Alonso',
-      position: '1B',
-      age: 30,
-      photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/37498.png&w=350&h=254'
-    },
-    {
-      firstName: 'Brandon',
-      lastName: 'Nimmo',
-      position: 'CF',
-      age: 32,
-      photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/32159.png&w=350&h=254'
-    },
-    {
-      firstName: 'Starling',
-      lastName: 'Marte',
-      position: 'RF',
-      age: 36,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/31127.png'
-    },
-    {
-      firstName: 'Jeff',
-      lastName: 'McNeil',
-      position: '2B',
-      age: 33,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/36062.png'
-    },
-    {
-      firstName: 'Francisco',
-      lastName: 'Álvarez',
-      position: 'C',
-      age: 23,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916460.png'
-    },
-    {
-      firstName: 'Brett',
-      lastName: 'Baty',
-      position: '3B',
-      age: 25,
-      photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/42414.png&w=350&h=254'
-    },
-    {
-      firstName: 'Luisangel',
-      lastName: 'Acuña',
-      position: 'INF',
-      age: 23,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916462.png'
-    },
-    {
-      firstName: 'Mark',
-      lastName: 'Vientos',
-      position: 'DH',
-      age: 25,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916463.png'
-    },
-    {
-      firstName: 'Tyrone',
-      lastName: 'Taylor',
-      position: 'OF',
-      age: 31,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/34886.png'
-    },
-    {
-      firstName: 'José',
-      lastName: 'Azocar',
-      position: 'OF',
-      age: 28,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916464.png'
-    },
-    {
-      firstName: 'Juan',
-      lastName: 'Soto',
-      position: 'OF',
-      age: 26,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/40921.png'
-    },
-    {
-      firstName: 'Kodai',
-      lastName: 'Senga',
-      position: 'SP',
-      age: 32,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916465.png'
-    },
-    {
-      firstName: 'David',
-      lastName: 'Peterson',
-      position: 'SP',
-      age: 29,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916466.png'
-    },
-    {
-      firstName: 'Griffin',
-      lastName: 'Canning',
-      position: 'SP',
-      age: 29,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916467.png'
-    },
-    {
-      firstName: 'Edwin',
-      lastName: 'Díaz',
-      position: 'RP',
-      age: 31,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/35394.png'
-    },
-    {
-      firstName: 'Ryne',
-      lastName: 'Stanek',
-      position: 'RP',
-      age: 33,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916468.png'
-    },
-    {
-      firstName: 'Reed',
-      lastName: 'Garrett',
-      position: 'RP',
-      age: 32,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916469.png'
-    },
-    {
-      firstName: 'José',
-      lastName: 'Buttó',
-      position: 'RP',
-      age: 27,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/4916470.png'
-    },
-    {
-      firstName: 'Génesis',
-      lastName: 'Cabrera',
-      position: 'RP',
-      age: 28,
-      photo: 'https://a.espncdn.com/i/headshots/mlb/players/full/41061.png'
-    }
-  ]
+  {
+    firstName: 'Rafael',
+    lastName: 'Nadal',
+    country: 'Spain',
+    age: 37,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/Rafael_Nadal_2018_3.jpg'
+  },
+  {
+    firstName: 'Novak',
+    lastName: 'Djokovic',
+    country: 'Serbia',
+    age: 36,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Novak_Djokovic_%28Serbia%29_2019_1.jpg'
+  },
+  {
+    firstName: 'Roger',
+    lastName: 'Federer',
+    country: 'Switzerland',
+    age: 41,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/8/8a/Roger_Federer_%28Halle_2019%29_01.jpg'
+  },
+  {
+    firstName: 'Serena',
+    lastName: 'Williams',
+    country: 'USA',
+    age: 41,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Serena_Williams_at_2013_US_Open_6_cropped.jpg'
+  },
+  {
+    firstName: 'Naomi',
+    lastName: 'Osaka',
+    country: 'Japan',
+    age: 25,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/5/57/Naomi_Osaka_US_Open_2018.jpg'
+  },
+  {
+    firstName: 'Andy',
+    lastName: 'Murray',
+    country: 'UK',
+    age: 36,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Andy_Murray_2016.jpg'
+  },
+  {
+    firstName: 'Simona',
+    lastName: 'Halep',
+    country: 'Romania',
+    age: 31,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Simona_Halep_Roland_Garros_2018_3_%28cropped%29.jpg'
+  },
+  {
+    firstName: 'Dominic',
+    lastName: 'Thiem',
+    country: 'Austria',
+    age: 29,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Dominic_Thiem_Roland_Garros_2018_1.jpg'
+  },
+  {
+    firstName: 'Ashleigh',
+    lastName: 'Barty',
+    country: 'Australia',
+    age: 26,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/c/c6/Ashleigh_Barty_2019.jpg'
+  },
+  {
+    firstName: 'Alexander',
+    lastName: 'Zverev',
+    country: 'Germany',
+    age: 26,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Alexander_Zverev_%28ATP_500_Halle_2018%29.jpg'
+  },
+  {
+    firstName: 'Venus',
+    lastName: 'Williams',
+    country: 'USA',
+    age: 43,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/7/77/Venus_Williams_Wimbledon_2013.jpg'
+  },
+  {
+    firstName: 'Stefanos',
+    lastName: 'Tsitsipas',
+    country: 'Greece',
+    age: 24,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/4/43/Stefanos_Tsitsipas_2019_2.jpg'
+  },
+  {
+    firstName: 'Caroline',
+    lastName: 'Wozniacki',
+    country: 'Denmark',
+    age: 31,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Caroline_Wozniacki_Wimbledon_2011_2.jpg'
+  },
+  {
+    firstName: 'Nick',
+    lastName: 'Kyrgios',
+    country: 'Australia',
+    age: 28,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Nick_Kyrgios_BNP_Paribas_Open_2015_1.jpg'
+  },
+  {
+    firstName: 'Maria',
+    lastName: 'Sharapova',
+    country: 'Russia',
+    age: 35,
+    photo: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Maria_Sharapova_Wimbledon_2013.jpg'
+  }
+];
